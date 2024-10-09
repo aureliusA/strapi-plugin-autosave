19afe958aebe9daad9d1b2e9e23e944031afa226
vadym <teoruz6@gmail.com>
2024-10-09 16:25:39 +0300

    - plugin name update

--- admin/src/pluginId.js
+++ admin/src/pluginId.js
@@ -1,1 +1,5 @@
-export const PLUGIN_ID = 'autosave';
+const pluginPkg = require('../../package.json');
+
+const pluginId = pluginPkg.name.replace(/^strapi-plugin-/i, '');
+
+module.exports = pluginId;
