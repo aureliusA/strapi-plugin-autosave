ce17b4aced803a2bc77ff4a55b6da53dbb63b17b
vadym <teoruz6@gmail.com>
2024-10-09 18:42:56 +0300

    - fix

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,4 +1,4 @@
-import { PLUGIN_ID } from './pluginId';
+import PLUGIN_ID from './pluginId';
 import AutoSaveComponent from "./components/AutoSave";
 
 export default {
--- admin/src/pluginId.js
+++ admin/src/pluginId.js
@@ -2,4 +2,4 @@ const pluginPkg = require('../../package.json');
 
 const pluginId = pluginPkg.name.replace(/^strapi-plugin-/i, '');
 
-module.exports = pluginId;
+export default pluginId;
