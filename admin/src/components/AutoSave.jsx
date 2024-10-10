96ec89e6d2e67246e3fff8a9f2a5c92c31dc0d3c
vadym <teoruz6@gmail.com>
2024-10-10 12:32:37 +0300

    - update

--- admin/src/components/AutoSave.jsx
+++ admin/src/components/AutoSave.jsx
@@ -22,6 +22,7 @@ const AutoSaveComponent = () => {
       timer = setTimeout(async () => {
         if (hasDataChanged()) {
           await put(`/content-manager/collection-types/${allLayoutData.contentType.uid}/${modifiedData.id}`, modifiedData);
+          document.querySelector('button[type="submit"]').setAttribute('aria-disabled', 'true');
         }
       }, 1000);
     }
